73fbcae73107d3c91969467135c95129e5878c95
itsrj23 <languban15@gmail.com>
2024-09-17 21:45:15 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace AreaOfRectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float Length = 0.0f;
+            float Breadth = 0.0f;
+            float Area = 0.0f;
+
+            
+            Console.WriteLine("---Rectangle Area Calculator---");
+            Console.WriteLine("");
+
+            //READ Length
+            Console.Write("Enter the length of the rectangle: ");
+            Length = float.Parse(Console.ReadLine());
+
+            //READ Breadth
+            Console.Write("Enter the breadth of the rectangle: ");
+            Breadth = float.Parse(Console.ReadLine());
+
+            //COMPUTE Area as Length * Breadth
+            Area = (Length * Breadth);
+
+            //PRINT Area
+            Console.WriteLine("The area of the rectangle is: " + Area);
+            Console.WriteLine("End of the Program");
+
+        }
+    }
+}
